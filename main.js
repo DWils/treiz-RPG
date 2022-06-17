dd7da889917c5d3f7769846ffb2c11c1ba17a833
unknown <alary.jordan62@gmail.com>
2022-06-17 11:51:05 +0200

    Test js

--- main.js
+++ main.js
@@ -26,4 +26,25 @@ var swiper = new Swiper(".mySwiper", {
     });
   }
   
-  document.querySelector('#myform').addEventListener('submit', addHero)
+  document.querySelector('#myform').addEventListener('btn-ajout', addHero);
+
+
+
+
+
+
+
+
+
+const carouselinner = document.querySelector(".carousel-inner");
+
+function CreerPerso(a){
+    if (a = document.querySelector('#myform').addEventListener("click","btn-ajout")){  // Si a est égale au clic fait sur le bouton
+        const divcarousel = document.createElement("div");
+        const carouselitem = divcarousel.createElement(".carousel-item");
+        carouselitem.innerHTML.document.querySelector("h5") = `${"#nom".value}`; // je veux qu'il s'intégre dans un h5
+        carouselitem.innerHTML.document.querySelector("h5") = `${"#classe".value}`; // je veux qu'il s'intégre dans le h5 avec le nom séparé d'un tiret
+        carouselitem.innerHTML.document.querySelector("img") = `${"#fichier".value}`; //je veux qu'il s'intégre dans un <img src="">
+        carouselinner.appendChild(carouselitem);
+    }
+}
